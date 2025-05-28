3e2a9aa1341b34924a5ffd6ad06cf89ebb7663f6
vondarkness <aroli.marcellinus@gmail.com>
2025-05-28 20:23:34 +0900

    Remove build from being tracked

--- build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_DrugSimulationGUI_FILE /home/marcell/Documents/Code/Qt/DrugSimulationGUI/build/Desktop-Debug/DrugSimulationGUI)
-set(__QT_DEPLOY_TARGET_DrugSimulationGUI_TYPE EXECUTABLE)
